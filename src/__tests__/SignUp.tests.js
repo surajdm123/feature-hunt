b046635568e8c5534e2d5cf81d88354070d72a91
Leila Moran <lmmoran@ncsu.edu>
2021-11-02 19:21:26 -0400

    Added test suite for SignUp.js
    * Tests signup render
    * Tests button clicks
    * Applied Prettier
    
    Co-Authored-By: Shraddha Mishra <7471821+shraddhamishra7@users.noreply.github.com>

--- src/__tests__/SignUp.tests.js
+++ src/__tests__/SignUp.tests.js
@@ -1,14 +1,6 @@
-/*
-
-LICENSE GOES HERE
-
-*/
-
 import React from "react";
 import { render, screen, fireEvent, getByTestId } from "@testing-library/react";
-import { Route as RRoute } from "react-router-dom";
-import { Router as RRouter } from "react-router-dom";
-import Router from "react-router-dom";
+import { Router as RRouter } from "react-router-dom"; // NOT A TYPO
 import { createMemoryHistory } from "history";
 import "@testing-library/jest-dom/extend-expect";
 
@@ -22,9 +14,8 @@ import "../setupTests";
  * GitHub repository: CSC510-Group-25/feature-hunt
  *
  * Authored by: Group 25
- *
- * NAME (GitHub ID: GHID)
- *
+ * Leila Moran (GitHub ID: snapcat)
+ * Shraddha Mishra (GitHub ID: shraddhamishra7)
  * */
 
 /*
@@ -46,31 +37,100 @@ data-testid="TEXT" -- short description
 */
 
 describe("SignUp tests", () => {
-  it("does things1", () => {
-    /*
-      const history = createMemoryHistory();
-      history.push('/:id');
-      const { getByTestId } = render(
-        <RRouter history={history}>
-          <Thing />
-        </RRouter>
-      );
-      const thing1 = getByTestId("thing1");
-      //expect(thing1.children.length).toBe(4);
-      */
+ 
+  it("renders signup", () => {
+    const history = createMemoryHistory();
+    history.push("/:id");
+    const { getByTestId, getByText, queryByText, queryByTestId, getByRole } = render(
+      <RRouter history={history}>
+        <SignUp />
+      </RRouter>
+    );
+
+    const signup = getByText(/signup/i);
+    expect(signup).toBeInTheDocument();
+
+    //const signupb = getByTestId("signup_button");
+    //fireEvent.click(signupb);
+
+    const signupb = getByRole("button", { name: /SignUp/i });
+    //expect(sub).toBeInTheDocument();
+
+    fireEvent.click(signupb);
+
+    const signtext = getByText(/Enter your name, email and password to get started with Feature Hunt!/i);
+    expect(signtext).toBeInTheDocument();
+
+    const placeholdername = getByText("Name");
+    expect(placeholdername).toBeInTheDocument();
+
+    const placeholderemail = getByText("Email Address");
+    expect(placeholderemail).toBeInTheDocument();
+
+    const placeholderpass = getByText("Password");
+    expect(placeholderpass).toBeInTheDocument();
+
+    const cancel = getByText(/Cancel/i);
+    const submit = getByText(/submit/i);
+
+    expect(cancel).toBeInTheDocument();
+    expect(submit).toBeInTheDocument();
   });
 
-  it("does things2", () => {
-    /*
-      const history = createMemoryHistory();
-      history.push('/:id');
-      const { getByTestId } = render(
+  it("tests signing up", () => {
+    const history = createMemoryHistory();
+    history.push("/:id");
+    const { getByTestId, getByText, getByRole, queryByText } =
+      render(
         <RRouter history={history}>
-          <Thing />
+          <SignUp />
         </RRouter>
       );
-      const thing1 = getByTestId("thing1");
-      //expect(thing1.children.length).toBe(4);
-      */
+
+    //const si = getByTestId("login_button");
+    //fireEvent.click(loginb);
+
+    const signupb = getByRole("button", { name: /SignUp/i });
+    //expect(sub).toBeInTheDocument();
+
+    fireEvent.click(signupb);
+
+    const sub = getByRole("button", { name: /Submit/i });
+    expect(sub).toBeInTheDocument();
+
+    const can = getByRole("button", { name: /Cancel/i });
+    expect(can).toBeInTheDocument();
+
+    fireEvent.click(can);
+    fireEvent.click(signupb);
+
+    const name = getByTestId("signup_inputName");
+    const nameval = "Test Name"
+
+    const add = getByTestId("signup_inputEmail");
+    const ress = "test@test.com";
+
+    const pass = getByTestId("signup_inputPassword");
+    const word = "abc";
+
+    expect(name).toHaveValue("");
+    expect(pass).toHaveValue("");
+    expect(add).toHaveValue("");
+
+    fireEvent.change(name, { target: { value: nameval } });
+    fireEvent.change(pass, { target: { value: word } });
+    fireEvent.change(add, { target: { value: ress } });
+
+    fireEvent.click(sub);
+
+    expect(name).toHaveValue(nameval);
+    expect(pass).toHaveValue(word);
+    expect(add).toHaveValue(ress);
+
+    // const err = screen.getByText(/Error: /i);
+    // expect(err).toBeInTheDocument();
+    // Error: We are unable to find a user with that email. Please double check you entered your email correctly
+    // Error: Password is incorrect
   });
+
 });
