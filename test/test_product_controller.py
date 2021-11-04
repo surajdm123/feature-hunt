33a6974046c740887f15340e06f815511ac5e5ba
jhnguye4 <jhnguye4@ncsu.edu>
2021-11-04 15:36:20 -0400

    documented test cases for submit project

--- test/test_product_controller.py
+++ test/test_product_controller.py
@@ -5,6 +5,14 @@ import pytest
 from flask import Flask
 import test_config
 
+#################################################################################
+##       Function: test_submit_project
+##       Description: Tests a valid name, description, imageUrl, tags, email and returns a 200 response
+##       Inputs:
+##           - NA
+##       Outputs:
+##           - NA
+#################################################################################
 def test_submit_project():
     mock_user = {'name': 'test_project',
                  'description': 'This is a test for project1',
@@ -17,6 +25,14 @@ def test_submit_project():
     response = requests.post(submit_project_url, data=mock_user)
     assert response.status_code == 200
 
+#################################################################################
+##       Function: test_submit_project_missing_name
+##       Description: Tests a valid, description, imageUrl, tags, email but an invalid name and returns a 403 response
+##       Inputs:
+##           - NA
+##       Outputs:
+##           - Assert response
+#################################################################################
 def test_submit_project_missing_name():
     mock_user = {'name': '',
                  'description': 'This is a test for project1',
@@ -29,6 +45,14 @@ def test_submit_project_missing_name():
     response = requests.post(submit_project_url, data=mock_user)
     assert response.status_code == 403
 
+#################################################################################
+##       Function: test_submit_project_missing_description
+##       Description: Tests a valid name, imageUrl, tags, email but an invalid description and returns a 403 response
+##       Inputs:
+##           - NA
+##       Outputs:
+##           - Assert response
+#################################################################################
 def test_submit_project_missing_description():
     mock_user = {'name': 'test_project',
                  'description': '',
@@ -41,6 +65,14 @@ def test_submit_project_missing_description():
     response = requests.post(submit_project_url, data=mock_user)
     assert response.status_code == 403
 
+#################################################################################
+##       Function: test_submit_project_missingUrl
+##       Description: Tests a valid name, description, tags, email but an invalid imageUrl and returns a 403 response
+##       Inputs:
+##           - NA
+##       Outputs:
+##           - Assert response
+#################################################################################
 def test_submit_project_missing_imageUrl():
     mock_user = {'name': 'test_project',
                  'description': 'This is a test for project1',
@@ -53,6 +85,14 @@ def test_submit_project_missing_imageUrl():
     response = requests.post(submit_project_url, data=mock_user)
     assert response.status_code == 403
 
+#################################################################################
+##       Function: test_submit_project_missing_tags
+##       Description: Tests a valid name,description, imageUrl, email but invalid tags and returns a 403 response
+##       Inputs:
+##           - NA
+##       Outputs:
+##           - Assert response
+#################################################################################
 def test_submit_project_missing_tags():
     mock_user = {'name': 'test_project',
                  'description': 'This is a test for project1',
@@ -65,6 +105,14 @@ def test_submit_project_missing_tags():
     response = requests.post(submit_project_url, data=mock_user)
     assert response.status_code == 403
 
+#################################################################################
+##       Function: test_submit_project_missing_email
+##       Description: Tests a valid name, description, tags, imageUrl but an invalid email and returns a 403 response
+##       Inputs:
+##           - NA
+##       Outputs:
+##           - Assert response
+#################################################################################
 def test_submit_project_missing_email():
     mock_user = {'name': 'test_project',
                  'description': 'This is a test for project1',
