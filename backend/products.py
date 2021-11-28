5358511fcee5597ef0c7be0a5d1c920ed6ed8f24
Sneha Aradhey <aradheysv@gmail.com>
2021-11-27 21:40:15 -0500

    fixed add feature issue.

--- backend/products.py
+++ backend/products.py
@@ -7,12 +7,11 @@ this file. If not, please write to: featurehuntteam@gmail.com
 
 # pylint: disable=wrong-import-position,poiackend-lontless-string-statement,undefined-variable,line-too-long
 
-import os
-from sys import stderr
-from flask import request, jsonify
+import os, io
+from flask import request, jsonify, Response
 from flask import json
 from app import app
-
+import pandas as pd
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from db_init import product_records
@@ -40,7 +39,26 @@ def products():
             return Response(response=json.dumps({"Error": "Please provide all necessary input"}),
                             status=400,
                             mimetype='application/json')
-        return jsonify({'ok': True, 'message': 'Product added successfully'}), 200
+        try:
+            product_name = request.form.get("productName")
+            product_description = request.form.get("productDescription")
+            image_url = request.form.get("imageUrl")
+            email = request.form.get("email")
+            last_date = request.form.get("lastDate")
+            tags = request.form.get("tags").split(' ')
+
+            feature_dict = {'id': 2, 'text': 'feature-1', 'votes': 1, 'timestamp': '1234567', 'tags': ['tag1']}
+
+            product_input = {'name': product_name, 'description': product_description, 'votes': 1,
+                             'image_url': image_url, 'users': [email], 'tags': tags, 'features': feature_dict,
+                             'last_date': last_date}
+
+
+            product_records.insert_one(product_input)
+
+            return jsonify({'ok': True, 'message': 'Product added successfully'}), 200
+        except:
+            return jsonify(success=False)
 
     if request.method == 'DELETE':
         if data is None or data == {}:
@@ -67,7 +85,21 @@ def products():
 def get_feature(productname):
     ''' see above '''
     if request.method == 'GET':
-        data = product_records.find({"name":productname})
+        data = product_records.find({"name": productname})
+    if request.method == 'POST':
+            data = request.json
+            data['_id'] = ObjectId()
+            print(data)
+            if data is None or data == {}:
+                return Response(response=json.dumps({"Error":
+                                "Please provide connection information"}),
+                                status=400,
+                                mimetype='application/json')
+            result = product_records.find_one_and_update(
+                {"project_name": productname}, {"$push": {"features": data}}
+            )
+
+            return jsonify(success=True)
     return dumps(data)
 
 '''
@@ -80,7 +112,7 @@ Outputs:
 '''
 
 
-# @app.route('/<product_name>/getFeature', methods=['GET', 'POST'])
+# @app.route('/<productname>/getFeature', methods=['GET', 'POST'])
 # def get_feature(product_name):
 #     if request.method == 'GET':
 #         data = product_records.find({"name": product_name})
@@ -97,8 +129,8 @@ Outputs:
 '''
 
 
-@app.route('/<product_name>/features', methods=['GET', 'POST'])
-def features(product_name):
+@app.route('/<productname>/features', methods=['GET', 'POST'])
+def features(productname):
     result = ''
     if request.method == 'POST':
         data = request.form.get('features')
@@ -108,20 +140,11 @@ def features(product_name):
             return Response(response=json.dumps({"Error": "Please provide connection information"}),
                             status=400,
                             mimetype='application/json')
-        result = product_records.find_one_and_update({"name": productname}, {"$set": {"features": data}})
+        result = product_records.find_one_and_update({"name": productname}, {"$set": {"features": data}},
+                                                     {'$upsert': 'true'})
 
     elif request.method == 'GET':
-        result = product_records.find({"name": product_name}, {"features": 1})
+        result = product_records.find({"name": productname}, {"features": 1})
     return dumps(result)
 
-
-@app.route('/feedback', methods=['GET', 'POST'])
-def feedback():
-    response = ''
-    return jsonify(response), 200
-
-# @app.route('/feature-hunt', methods=['GET', 'POST'])
-# def roadmap():
-#     response = ''
-#     return jsonify(response), 200
 
